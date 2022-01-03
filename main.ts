f54ec26ffc5f2e56319f1e793919f62100d8e0d1
Elindh10 <97030893+Elindh10@users.noreply.github.com>
2022-01-03 13:32:22 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("ERIC")
+    basic.showString("STELLA")
+    basic.showString("VINCENT")
+    basic.showString("SOFIA")
+    basic.showString("LARS")
+})
